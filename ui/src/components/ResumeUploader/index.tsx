bec04a5b3ddd22e6da140742ced201a15154665a
Laxmi Malpani <laxmi.malpani@latentview.com>
2025-10-05 03:33:09 -0700

    upload changes

--- ui/src/components/ResumeUploader/index.tsx
+++ ui/src/components/ResumeUploader/index.tsx
@@ -1,23 +1,20 @@
-
-
-
 // components/ResumeUploader.tsx
 import React, { useState, ChangeEvent, FormEvent } from "react";
 import { MatchScoreResponse } from "../../types/resume";
-import './index.css';
-import { useUploadResumeMutation } from '../../redux/api';
-import { useNavigate } from 'react-router-dom';
-import { useAppDispatch } from '../../hooks';
-import { setMatchData } from '../../redux/features/match/matchSlice';
+import "./index.css";
+import { useUploadResumeMutation } from "../../redux/api";
+import { useNavigate } from "react-router-dom";
+import { useAppDispatch } from "../../hooks";
+import { setMatchData } from "../../redux/features/match/matchSlice";
 
-const ResumeUploader= () => {
+const ResumeUploader = () => {
   const navigate = useNavigate();
-  const dispatch = useAppDispatch();;
+  const dispatch = useAppDispatch();
   const [loading, setLoading] = useState<boolean>(false);
   const [resumeFile, setResumeFile] = useState<File | null>(null);
-  const [resumeText, setResumeText] = useState<string>('');
+  const [resumeText, setResumeText] = useState<string>("");
   const [jdFile, setJdFile] = useState<File | null>(null);
-  const [jdText, setJdText] = useState<string>('');
+  const [jdText, setJdText] = useState<string>("");
   const [uploadResume, { isLoading }] = useUploadResumeMutation();
 
   const handleFileChange = (
@@ -27,26 +24,24 @@ const ResumeUploader= () => {
   ) => {
     const file = e.target.files?.[0];
     if (!file) return;
-    setterFile(file)
+    setterFile(file);
   };
 
-
   const readFileAsText = (file: File): Promise<string> => {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
 
-    reader.onload = () => {
-      resolve(reader.result as string);
-    };
+      reader.onload = () => {
+        resolve(reader.result as string);
+      };
 
-    reader.onerror = () => {
-      reject(reader.error);
-    };
+      reader.onerror = () => {
+        reject(reader.error);
+      };
 
-    reader.readAsText(file); // Read as plain text
-  });
-};
-
+      reader.readAsText(file); // Read as plain text
+    });
+  };
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -55,76 +50,75 @@ const ResumeUploader= () => {
       alert("Please provide both resume and job description.");
       return;
     }
-    let jobdesc : any= null
+    let jobdesc: any = null;
 
-    if (jdFile.type === 'application/pdf') {
-        alert("Job desc should be text")
-        return 
-    } else if (jdFile.type === 'text/plain') {
+    if (jdFile.type === "application/pdf") {
+      alert("Job desc should be text");
+      // return
+    } else if (jdFile.type === "text/plain") {
       const text = await readFileAsText(jdFile);
-      jobdesc = text
-      console.log("jobdesc", jobdesc)
+      jobdesc = text;
+      console.log("jobdesc", jobdesc);
     } else {
-      alert('Only PDF or TXT files are allowed.');
+      alert("Only PDF or TXT files are allowed.");
     }
 
     const formData = new FormData();
-    formData.append('resume_file', resumeFile);
-    formData.append('jd_text', jobdesc);
+    formData.append("resume_file", resumeFile);
+    // formData.append("jd_text", jdFile);
+    formData.append("jd_text", jobdesc);
+
     try {
-      setLoading(true)
+      setLoading(true);
       const response: any = await uploadResume(formData).unwrap();
-            setLoading(false)
+      setLoading(false);
 
       dispatch(setMatchData(response)); // Store the response
-      navigate('/match-score');
+      navigate("/match-score");
     } catch (error) {
-      console.error('Upload failed:', error);
+      console.error("Upload failed:", error);
     }
-
   };
 
   return (
-     <div className="uploader-container">
+    <div className="uploader-container">
       <h2>Resume Matcher</h2>
       {/* Resume Input */}
       <form onSubmit={handleSubmit}>
-      <div className="input-group">
-        <label>Resume (PDF/TXT):</label>
-        <input
-          id="resume-input"
-          type="file"
-          accept=".pdf,.txt"
-          onChange={(e) => handleFileChange(e, setResumeFile, setResumeText)}
-        />
-        {resumeFile && <p className="file-name">📄 {resumeFile.name}</p>}
-      </div>
+        <div className="input-group">
+          <label>Resume (PDF/TXT):</label>
+          <input
+            id="resume-input"
+            type="file"
+            accept=".pdf,.txt"
+            onChange={(e) => handleFileChange(e, setResumeFile, setResumeText)}
+          />
+          {resumeFile && <p className="file-name">📄 {resumeFile.name}</p>}
+        </div>
 
-      {/* JD Input */}
-      <div className="input-group">
-        <label>Job Description (TXT):</label>
-        <input
-          id="jd-input"
-          type="file"
-          accept=".pdf,.txt"
-          onChange={(e) => handleFileChange(e, setJdFile, setJdText)}
-        />
-        {jdFile && <p className="file-name">📄 {jdFile.name}</p>}
-      </div>
+        {/* JD Input */}
+        <div className="input-group">
+          <label>Job Description (TXT):</label>
+          <input
+            id="jd-input"
+            type="file"
+            accept=".pdf,.txt"
+            onChange={(e) => handleFileChange(e, setJdFile, setJdText)}
+          />
+          {jdFile && <p className="file-name">📄 {jdFile.name}</p>}
+        </div>
 
-      {/* Buttons */}
-      {/* <div className="button-group">
+        {/* Buttons */}
+        {/* <div className="button-group">
         <button className="cancel-btn" onClick={handleCancel}>Cancel</button>
         <button className="submit-btn" onClick={handleSubmit}>Submit</button>
       </div> */}
 
-         <button className="submit-btn" type="submit" disabled={loading}>
-        {loading ? "Processing..." : "Submit"}
-      </button>
+        <button className="submit-btn" type="submit" disabled={loading}>
+          {loading ? "Processing..." : "Submit"}
+        </button>
       </form>
     </div>
-
-
   );
 };
 
---
+++ ui/src/pages/Signin/index.tsx
@@ -0,0 +1,191 @@
+import { useRef } from "react";
+import logo from "../../assets/images/logo.png";
+import lv_logo from "../../assets/images/lv_logo_img.png";
+import google from "../../assets/icons/google.svg";
+import background from "../../assets/video/background.mp4";
+// import useAuth from "../../hooks/useAuth";
+// import CircularSpinner from "../../components/CircularSpinner";
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
+
+export default function Signin() {
+  // const { isLoading } = useAuth();
+  // const { user } = useSelector((state: RootState) => state.loginUser);
+  const videoRef = useRef(null);
+
+  const signin = () =>
+    (window.location.href = `${import.meta.env.VITE_API_URL}auth/google`);
+
+  // if (isLoading) {
+  //   return (
+  //     <CircularSpinner
+  //       loader={isLoading}
+  //       backgroundColor="white"
+  //       border="#9061f9"
+  //     />
+  //   );
+  //  }
+
+  return (
+    <>
+      <div className="relative h-screen w-screen overflow-hidden">
+        {/* Background video */}
+        <video
+          ref={videoRef}
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="absolute top-0 left-0 w-full h-full object-cover"
+        >
+          <source src={background} type="video/mp4" />
+        </video>
+
+        {/* Overlay */}
+        <div className="absolute inset-0 bg-black/40"></div>
+
+        {/* Logo */}
+
+        <div
+          className="absolute  inline-flex items-end "
+          style={{
+            top: "clamp(21.3px, 2.778vw, 100px)",
+            left: "clamp(19.2px, 2.5vw, 100px)",
+          }}
+        >
+          <div className="flex">
+            <img
+              src={lv_logo}
+              alt="LatentView Logo"
+              className="object-contain block"
+              style={{
+                width: "clamp(89px, 11.58vw, 150px)",
+              }}
+            />
+            &nbsp;&nbsp;
+            <span
+              className="text-sky-500 font-poppins font-normal self-end mb-[1px] 5xl: mb-[3px]"
+              style={{ fontSize: "clamp(10.7px, 1.389vw, 52px)" }}
+            >
+              x
+            </span>
+            &nbsp;
+            <span
+              className="text-white font-poppins font-normal self-end mb-[1px] 5xl: mb-[3px]"
+              style={{ fontSize: "clamp(10.7px, 1.389vw, 52px)" }}
+            >
+              CTech
+            </span>
+          </div>
+        </div>
+
+        {/* Login Card */}
+
+        <div
+          className="absolute top-1/2 -translate-y-1/2 flex flex-col items-center justify-center
+                        backdrop-blur-2xl border border-white/20 rounded-[32px] shadow-xl
+                        bg-[#152D474D] h-auto  "
+          style={{
+            width: "clamp(300px, 28vw, 100vw)",
+            height: "clamp(247px, 32.22vw, 100vw)",
+            right: "clamp(25px, 3.33vw, 200px)",
+          }}
+        >
+          <div className="my-[67px] flex flex-col items-center justify-center">
+            {/* Logo */}
+            <img
+              src={logo}
+              alt="LatentView Logo"
+              className="object-contain"
+              style={{
+                width: "clamp(40px, 3.4vw, 200px)",
+                height: "clamp(36px, 3.1vw, 200px)",
+              }}
+            />
+
+            {/* Heading */}
+
+            {/* 32px */}
+            <h2
+              className="text-white text-center mt-3 font-poppins font-medium leading-[100%] tracking-[0%]"
+              style={{ fontSize: "clamp(18px, 2.22vw, 100px)" }}
+            >
+              GrowthOps <span className="text-sky-400">AI</span>
+            </h2>
+
+            {/* Description */}
+
+            {/* mt-30px px-5 */}
+            <div
+              className="text-[#dedede] text-center"
+              style={{
+                marginTop: "clamp(16px, 2.08vw, 100px)",
+                paddingLeft: "clamp(10px, 1.39vw, 100px)",
+                paddingRight: "clamp(10px, 1.39vw, 100px)",
+              }}
+            >
+              {/* text-12px */}
+              <span
+                className="font-manrope font-normal leading-relaxed"
+                style={{ fontSize: "clamp(8px, 0.833vw, 52px)" }}
+              >
+                A GraphDB-powered chatbot for exploring and understanding
+                complex GrowthOps relationships and insights.
+              </span>
+            </div>
+
+            {/* Google Sign-in Button */}
+            <button
+              onClick={() => signin()}
+              className="flex items-center justify-center bg-white rounded-md shadow hover:bg-gray-100 transition py-2 "
+              style={{
+                width: "clamp(177px, 20.05vw, 1500px)",
+                marginTop: "clamp(10px, 2vw, 200px)",
+                paddingTop: "clamp(8px, 0.56vw, 30px)",
+                paddingBottom: "clamp(8px, 0.56vw, 30px)",
+              }}
+            >
+              <img
+                src={google}
+                alt="Google"
+                className="
+                  mr-2"
+                style={{
+                  width: "clamp(13px, 1.67vw, 100px)",
+                  height: "clamp(13px, 1.67vw, 100px)",
+                }}
+              />
+              <span
+                className="font-roboto font-medium text-black leading-[100%] tracking-[0%]"
+                style={{ fontSize: "clamp(8px, 1.11vw, 52px)" }}
+              >
+                Sign in with Google
+              </span>
+            </button>
+          </div>
+        </div>
+
+        {/* style={{ top: `${taglineTop}px` }} */}
+        {/* top-[calc(50%+250px)] */}
+        <div
+          className=" fixed bottom-0 absolute -translate-y-1/2 flex flex-col items-center justify-center text-center
+                        text-white bg-[#00000066] backdrop-blur-2xl opacity-100
+                        rounded-[24px] p-[10px]"
+          style={{
+            width: "clamp(195px, 28vw, 1500px)",
+            height: "clamp(20px, 2.63vw, 400px)",
+            right: "clamp(25px, 3.33vw, 200px)",
+          }}
+        >
+          <p
+            className="font-poppins font-normal leading-[100%] tracking-[0%]"
+            style={{ fontSize: "clamp(6.4px, 0.833vw, 50px)" }}
+          >
+            Powered by Pragya AI & GraphDB | Built by Team Nirvana
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
